762114b06e7b99c7c64f1073f46cb12b5bd3069d
Leander Jedamus <ljedamus@web.de>
2018-05-07 18:45:06 +0200

    added several zstyle

--- config/09completion.zsh
+++ config/09completion.zsh
@@ -5,17 +5,31 @@ zmodload -i zsh/complist
 zstyle ':completion:*' verbose yes
 zstyle ':completion:*' group-name ''
 
+#lj2018
+zstyle ':completion:*' format %d
+zstyle ':completion:*:cd:*' ignore-parents parent pwd
+zstyle ':completion:*' squeeze-slashes true
+
 #lj2015:
-zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
-zstyle ':completion:*' show-ambiguity $color[bg-red]
+
+if [ $mactype != "MacOS" ];then
+  zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
+  zstyle ':completion:*' show-ambiguity $color[bg-red]
+fi
 
 zstyle ':completion:*' accept-exact false
 
-# reverse video für ...
-zstyle ':completion:*:descriptions' format '%S%d%s'
-zstyle ':completion:*:corrections' format '%S%d (errors: %e)%s'
-zstyle ':completion:*:messages' format '%S%d%s'
-zstyle ':completion:*:warnings' format '%S%d%s'
+if [ $mactype != "MacOS" ];then
+  # reverse video für ...
+  zstyle ':completion:*:descriptions' format '%S%d%s'
+  zstyle ':completion:*:corrections' format '%S%d (errors: %e)%s'
+  zstyle ':completion:*:messages' format '%S%d%s'
+  zstyle ':completion:*:warnings' format '%S%d%s'
+else
+  zstyle ':completion:*:descriptions' format '%B%d%b'
+  zstyle ':completion:*:messages' format %d
+  zstyle ':completion:*:warnings' format 'No matches for: %d'
+fi
 
 zstyle ':completion:*:(all-|)files' ignored-patterns \
   "(*.BAK|*.bak|*.o|*.aux|*.toc|*.idx|*.ind|*.ilg|*.lof|*.lot|*.swp|*~)"
@@ -25,17 +39,22 @@ zstyle ':completion:*:rm:*:(all-|)files' ignored-patterns
 
 zstyle ':completion:*' menu select=3
 
-zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
+if [ $mactype != "MacOS" ];then
+  zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
+fi
 
 # fehler tolerieren
 zstyle ':completion:*' completer _complete _correct _approximate
+zstyle ':completion::complete:*' use-cache on
 zstyle -e ':completion:*:approximate:*' max-errors \
   'reply=( $(( ($#PREFIX+$#SUFFIX)/3 )) numeric )'
 zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
 
 # von mir: ##+## evince einstellen
-zstyle ':completion:*:*:evince:*:*' file-patterns \
-  "(*.pdf|*.ps|*.dvi|*.png|*.gif)" "*(-/)"
+if [ $mactype != "MacOS" ];then
+  zstyle ':completion:*:*:evince:*:*' file-patterns \
+        "(*.pdf|*.ps|*.dvi|*.png|*.gif)" "*(-/)"
+fi
 
 zstyle ':completion:*:vi(|m):*:*' file-patterns '*.tex' '*.(c|cc|cpp|c++|h|hh|py|pl|sh|java|cob|f)' '*'
 

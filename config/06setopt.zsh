9226da81f993aace9d3f7a02864c92dcc7fcd71f
Leander Jedamus <ljedamus@web.de>
2018-05-07 11:52:21 +0200

    added INC_APPEND_HISTORY

--- config/06setopt.zsh
+++ config/06setopt.zsh
@@ -1,6 +1,7 @@
 setopt extended_glob
-# setopt share_history
 setopt append_history
+# setopt share_history
+# setopt inc_append_history
 setopt hist_reduce_blanks
 setopt hist_ignore_all_dups
 setopt hist_verify

25ef84832ea6aad1113994b03f381c14ef7f12ff
Leander Jedamus <ljedamus@web.de>
2020-10-03 03:35:19 +0200

    added a function fort

--- output/01fortune.zsh
+++ output/01fortune.zsh
@@ -1,20 +1,25 @@
 
 
 # erzeugt Samstag, 26. September 2020 19:29 (C) 2020 von Leander Jedamus
+# modifiziert Samstag, 03. Oktober 2020 03:32 von Leander Jedamus
 # modifiziert Samstag, 26. September 2020 21:24 von Leander Jedamus
 
-if [ $SHLVL -eq 1 ]; then
+fort() {
   FORTUNE=/usr/games/fortune
   NL="-n"
   if [ -x $FORTUNE ]; then
     echo -n "${fg[blue]}"
     #$FORTUNE -s
-    $FORTUNE linux
+    $FORTUNE $1
     echo -n "${fg[default]}"
     NL=""
   fi
   echo $NL
   unset FORTUNE NL
+};# fort()
+
+if [ $SHLVL -eq 1 ]; then
+  fort linux
 fi
 
 # vim:ai sw=2

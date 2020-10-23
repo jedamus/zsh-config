41da733e92f6c8da8648b96e913a09643e2565f9
Leander Jedamus <ljedamus@web.de>
2020-10-23 11:17:32 +0200

    added 17compdef.zsh

---
+++ config/17compdef.zsh
@@ -0,0 +1,9 @@
+
+
+# erzeugt Freitag, 23. Oktober 2020 11:13 (C) 2020 von Leander Jedamus
+# modifiziert Freitag, 23. Oktober 2020 11:15 von Leander Jedamus
+
+compdef '_files -g "*.log"' '-redirect-,2>,-default-'
+
+# vim:ft=zsh ai sw=2 ts=2 et
+

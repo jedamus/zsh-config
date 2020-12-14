f196934443a0fee9aed9654784c85212bfec67b0
Leander Jedamus <ljedamus@web.de>
2020-12-14 14:48:50 +0100

    minor changes

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
 
 # erzeugt Montag, 14. Dezember 2020 14:40 (C) 2020 von Leander Jedamus
-# modifiziert Montag, 14. Dezember 2020 14:41 von Leander Jedamus
+# modifiziert Montag, 14. Dezember 2020 14:48 von Leander Jedamus
 
-cp -vp .zsh/zshprompt.py $HOME/bin
-cp -vp .zsh/.zshrc .zsh/.myenv $HOME
+cp -vp zshprompt.py $HOME/bin
+cp -vp .zshrc .myenv $HOME
 
 # vim:ai sw=2
 

8eba72e6e54193cf268da9b0d34542f024f62ed4
Leander Jedamus <ljedamus@web.de>
2020-12-14 14:41:57 +0100

    added install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+# erzeugt Montag, 14. Dezember 2020 14:40 (C) 2020 von Leander Jedamus
+# modifiziert Montag, 14. Dezember 2020 14:41 von Leander Jedamus
+
+cp -vp .zsh/zshprompt.py $HOME/bin
+cp -vp .zsh/.zshrc .zsh/.myenv $HOME
+
+# vim:ai sw=2
+

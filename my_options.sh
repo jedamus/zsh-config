8959734079c50b95812c6404e80e99ca765b3abd
Test User <testuser@testuser.org>
2021-05-03 16:00:15 +0200

    added my_options.sh

---
+++ my_options.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+# erzeugt Mittwoch, 02. Dezember 2020 13:53 (C) 2020 von Leander Jedamus
+# modifiziert Donnerstag, 03. Dezember 2020 17:15 von Leander Jedamus
+# modifiziert Mittwoch, 02. Dezember 2020 13:58 von Leander Jedamus
+
+color_options="--color=auto"
+
+ls_options="$color_options"
+grep_options="-i $color_options"
+
+# vim:ai sw=2
+

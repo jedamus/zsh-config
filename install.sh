8597464a74ce54a8ad0b877242a5ed9de666fab6
Leander Jedamus <ljedamus@web.de>
2021-06-10 18:23:05 +0200

    changed to use color_options.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 # modifiziert Montag, 14. Dezember 2020 14:48 von Leander Jedamus
 
 mkdir -p $HOME/bin
-cp -vp zshprompt.py my_options.sh $HOME/bin
+cp -vp zshprompt.py color_options.sh $HOME/bin
 cp -vp .zshrc .shenv $HOME
 
 # vim:ai sw=2

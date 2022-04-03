7f3282a084cab364674055f2cd94ae98e65f3f53
Leander Jedamus <ljedamus@web.de>
2022-04-03 06:37:39 +0200

    changed install.sh to use python version 2 and 3

--- install.sh
+++ install.sh
@@ -1,10 +1,23 @@
 #!/usr/bin/env sh
 
 # erzeugt Montag, 14. Dezember 2020 14:40 (C) 2020 von Leander Jedamus
+# modifiziert Sonntag, 03. April 2022 06:34 von Leander Jedamus
 # modifiziert Montag, 14. Dezember 2020 14:48 von Leander Jedamus
 
 mkdir -p $HOME/bin
-cp -vp zshprompt.py color_options.sh $HOME/bin
+
+version=2
+if [ -x /usr/local/bin/python3 ]; then
+  echo "found python3"
+  version=3
+fi
+if [ -x /usr/bin/python3 ]; then
+  echo "found python3"
+  version=3
+fi
+
+cp -vp zshprompt$version.py $HOME/bin/zshprompt.py
+cp -vp color_options.sh $HOME/bin
 cp -vp .zshrc .shenv .shlvl $HOME
 
 # vim:ai sw=2

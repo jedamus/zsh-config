ff029e73ae0eaa7526beaf8d5bd6bd1b03dbc7cb
Leander Jedamus <jedamus@users.noreply.github.com>
2022-09-26 15:43:04 +0200

    added ipython

--- config/16set_title.zsh
+++ config/16set_title.zsh
@@ -1,6 +1,7 @@
 
 
 # erzeugt Samstag, 26. September 2020 19:29 (C) 2020 von Leander Jedamus
+# modifiziert Montag, 26. September 2022 15:38 von Leander Jedamus
 # modifiziert Samstag, 03. Oktober 2020 13:44 von Leander Jedamus
 # modifiziert Samstag, 26. September 2020 19:34 von Leander Jedamus
 
@@ -15,7 +16,7 @@ if [[ -n "$DISPLAY" ]]; then
     # set_title "${USER}@${HOST}:${PWD}"
   # };# chpwd()
   cd .
-  vagrant ssh telnet su sudo bash zsh csh tcsh screen tmux byobu docker()
+  vagrant ssh telnet su sudo bash zsh csh tcsh screen tmux byobu docker ipython()
   {
     set_title "$0 $@"
     {

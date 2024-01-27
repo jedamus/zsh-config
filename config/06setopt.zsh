0230a721d0f136f4c801e77056461e71657e1318
Leander Jedamus <ljedamus@web.de>
2024-01-27 14:43:09 +0100

    added setopt share_history and inc_append_history

--- config/06setopt.zsh
+++ config/06setopt.zsh
@@ -1,14 +1,15 @@
 
 
 # erzeugt Samstag, 26. September 2020 19:29 (C) 2020 von Leander Jedamus
+# modifiziert Samstag, 27. Januar 2024 14:39 von Leander Jedamus
 # modifiziert Samstag, 26. September 2020 20:49 von Leander Jedamus
 
 setopt nocaseglob
 setopt csh_null_glob
 setopt extended_glob
 setopt append_history
-# setopt share_history
-# setopt inc_append_history
+setopt share_history
+setopt inc_append_history
 setopt hist_reduce_blanks
 setopt hist_ignore_all_dups
 setopt hist_verify

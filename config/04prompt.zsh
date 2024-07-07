73c5a27b9c834beb18e9c38e7f5be9ad1aae04e2
Leander Jedamus <ljedamus@web.de>
2024-07-07 09:05:19 +0200

    added prompt for virtualenv

--- config/04prompt.zsh
+++ config/04prompt.zsh
@@ -1,6 +1,7 @@
 
 
 # erzeugt Samstag, 26. September 2020 19:29 (C) 2020 von Leander Jedamus
+# modifiziert Sonntag, 30. Juni 2024 06:10 von Leander Jedamus
 # modifiziert Samstag, 26. September 2020 20:49 von Leander Jedamus
 
 # colors
@@ -16,6 +17,8 @@ for i in ${(k)fg};fgp[$i]=%{${fg[$i]}%}
 typeset -A bgp
 for i in ${(k)bg};bgp[$i]=%{${bg[$i]}%}
 
+# Display a "we are in a virtualenv" indicator that works in child shells too
+VIRTUAL_ENV_DISABLE_PROMPT=1
 export PS1="${fgp[yellow]}${bgp[blue]}z${fgp[blue]}${bgp[yellow]}%n@%m${fgp[default]}${bgp[default]} %T (%!) %(!.#.$) ";
 export RPROMPT="${fgp[green]}%4~${fgp[default]}${fgp[red]}%(?.. :( %?)${fgp[default]}";
 
--- config/14pythonprompt.zsh
+++ config/14pythonprompt.zsh
@@ -1,18 +1,36 @@
 
 
 # erzeugt Samstag, 26. September 2020 19:29 (C) 2020 von Leander Jedamus
+# modifiziert Sonntag, 07. Juli 2024 09:02 von Leander Jedamus
+# modifiziert Sonntag, 30. Juni 2024 06:16 von Leander Jedamus
 # modifiziert Samstag, 26. September 2020 20:51 von Leander Jedamus
+
+virtualenv_prompt() {
+  # If not in a virtualenv, print nothing
+  [[ "${VIRTUAL_ENV}" == "" ]] && return
+
+  # Distinguish between the shell where the virtualenv was activated
+  # and its children
+  local venv_name="${VIRTUAL_ENV##*/}"
+  if typeset -f deactivate >/dev/null; then
+    echo "[${venv_name}] "
+  else
+    echo "<${venv_name}> "
+  fi
+}
 
 export OLDPROMPT=$PROMPT
 export OLDRPROMPT=$RPROMPT
 precmd() {
+  PS1="$(virtualenv_prompt)${fgp[yellow]}${bgp[blue]}z${fgp[blue]}${bgp[yellow]}%n@%m${fgp[default]}${bgp[default]} %T (%!) %(!.#.$) "
   stat=$?
   prmt=$HOME/bin/zshprompt.py
   if [[ -d ./.git ]]; then
     export PROMPT="${$($prmt left):-$PROMPT}"
     export RPROMPT="${$($prmt right --last-exit-status $stat):-$RPROMPT}"
   else
-    export PROMPT=$OLDPROMPT
+    #export PROMPT=$OLDPROMPT
+    export PROMPT="$(virtualenv_prompt)${fgp[yellow]}${bgp[blue]}z${fgp[blue]}${bgp[yellow]}%n@%m${fgp[default]}${bgp[default]} %T (%!) %(!.#.$) "
     export RPROMPT=$OLDRPROMPT
   fi
 }
